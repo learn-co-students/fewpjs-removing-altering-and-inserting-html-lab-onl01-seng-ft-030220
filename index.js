2bac96d7f49904e221cd3b9ae39413977049ab12
007steve <stephen.plummer1.sp@gmail.com>
2020-06-05 00:20:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let element = document.querySelector('main#main')
+element.remove()
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Arv is the champion";
