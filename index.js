bb0e247bfcc7803db4ed96c55f42cf4997476a4e
TerrenceBell <terrencekbell@gmail.com>
2020-06-06 10:10:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code here!\let ul = document.createElement('ul')
+ 
+const q = document.querySelector('main')
+q.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
